b9e88c26c54cfc99be16203a23f71d5c5e4bd5dc
Werner Van Geit <werner.vangeit@epfl.ch>
2015-09-01 11:49:35 +0200

    Switch to hbp theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -100,7 +100,7 @@ tolerate_sphinx_warnings = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'haiku-bbp'
+html_theme = 'hbp-collaboratory-sphinx-theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
